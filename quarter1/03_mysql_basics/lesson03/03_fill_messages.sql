aa1b0aad7718f2cabe178e0c1f2f82517e12741f
rm-develop <rm-develop@yandex.ru>
2021-09-13 00:04:43 +0500

    q1_sql_lesson3_hw

---
+++ quarter1/03_mysql_basics/lesson03/03_fill_messages.sql
@@ -0,0 +1,134 @@
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'ACADEMY DINOSAUR: A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies','2006-02-15 05:03:42'),
+	 (1,2,'ACE GOLDFINGER: A Astounding Epistle of a Database Administrator And a Explorer who must Find a Car in Ancient China','2006-02-15 05:03:42'),
+	 (1,2,'ADAPTATION HOLES: A Astounding Reflection of a Lumberjack And a Car who must Sink a Lumberjack in A Baloon Factory','2006-02-15 05:03:42'),
+	 (1,2,'AFFAIR PREJUDICE: A Fanciful Documentary of a Frisbee And a Lumberjack who must Chase a Monkey in A Shark Tank','2006-02-15 05:03:42'),
+	 (1,2,'AFRICAN EGG: A Fast-Paced Documentary of a Pastry Chef And a Dentist who must Pursue a Forensic Psychologist in The Gulf of Mexico','2006-02-15 05:03:42'),
+	 (1,2,'AGENT TRUMAN: A Intrepid Panorama of a Robot And a Boy who must Escape a Sumo Wrestler in Ancient China','2006-02-15 05:03:42'),
+	 (1,2,'AIRPLANE SIERRA: A Touching Saga of a Hunter And a Butler who must Discover a Butler in A Jet Boat','2006-02-15 05:03:42'),
+	 (1,2,'AIRPORT POLLOCK: A Epic Tale of a Moose And a Girl who must Confront a Monkey in Ancient India','2006-02-15 05:03:42'),
+	 (1,2,'ALABAMA DEVIL: A Thoughtful Panorama of a Database Administrator And a Mad Scientist who must Outgun a Mad Scientist in A Jet Boat','2006-02-15 05:03:42'),
+	 (1,2,'ALADDIN CALENDAR: A Action-Packed Tale of a Man And a Lumberjack who must Reach a Feminist in Ancient China','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'ALAMO VIDEOTAPE: A Boring Epistle of a Butler And a Cat who must Fight a Pastry Chef in A MySQL Convention','2006-02-15 05:03:42'),
+	 (1,2,'ALASKA PHANTOM: A Fanciful Saga of a Hunter And a Pastry Chef who must Vanquish a Boy in Australia','2006-02-15 05:03:42'),
+	 (1,2,'ALI FOREVER: A Action-Packed Drama of a Dentist And a Crocodile who must Battle a Feminist in The Canadian Rockies','2006-02-15 05:03:42'),
+	 (1,2,'ALICE FANTASIA: A Emotional Drama of a A Shark And a Database Administrator who must Vanquish a Pioneer in Soviet Georgia','2006-02-15 05:03:42'),
+	 (1,2,'ALIEN CENTER: A Brilliant Drama of a Cat And a Mad Scientist who must Battle a Feminist in A MySQL Convention','2006-02-15 05:03:42'),
+	 (1,2,'ALLEY EVOLUTION: A Fast-Paced Drama of a Robot And a Composer who must Battle a Astronaut in New Orleans','2006-02-15 05:03:42'),
+	 (1,2,'ALONE TRIP: A Fast-Paced Character Study of a Composer And a Dog who must Outgun a Boat in An Abandoned Fun House','2006-02-15 05:03:42'),
+	 (1,2,'ALTER VICTORY: A Thoughtful Drama of a Composer And a Feminist who must Meet a Secret Agent in The Canadian Rockies','2006-02-15 05:03:42'),
+	 (1,2,'AMADEUS HOLY: A Emotional Display of a Pioneer And a Technical Writer who must Battle a Man in A Baloon','2006-02-15 05:03:42'),
+	 (1,2,'AMELIE HELLFIGHTERS: A Boring Drama of a Woman And a Squirrel who must Conquer a Student in A Baloon','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'AMERICAN CIRCUS: A Insightful Drama of a Girl And a Astronaut who must Face a Database Administrator in A Shark Tank','2006-02-15 05:03:42'),
+	 (1,2,'AMISTAD MIDSUMMER: A Emotional Character Study of a Dentist And a Crocodile who must Meet a Sumo Wrestler in California','2006-02-15 05:03:42'),
+	 (1,2,'ANACONDA CONFESSIONS: A Lacklusture Display of a Dentist And a Dentist who must Fight a Girl in Australia','2006-02-15 05:03:42'),
+	 (1,2,'ANALYZE HOOSIERS: A Thoughtful Display of a Explorer And a Pastry Chef who must Overcome a Feminist in The Sahara Desert','2006-02-15 05:03:42'),
+	 (1,2,'ANGELS LIFE: A Thoughtful Display of a Woman And a Astronaut who must Battle a Robot in Berlin','2006-02-15 05:03:42'),
+	 (1,2,'ANNIE IDENTITY: A Amazing Panorama of a Pastry Chef And a Boat who must Escape a Woman in An Abandoned Amusement Park','2006-02-15 05:03:42'),
+	 (1,2,'ANONYMOUS HUMAN: A Amazing Reflection of a Database Administrator And a Astronaut who must Outrace a Database Administrator in A Shark Tank','2006-02-15 05:03:42'),
+	 (1,2,'ANTHEM LUKE: A Touching Panorama of a Waitress And a Woman who must Outrace a Dog in An Abandoned Amusement Park','2006-02-15 05:03:42'),
+	 (1,2,'ANTITRUST TOMATOES: A Fateful Yarn of a Womanizer And a Feminist who must Succumb a Database Administrator in Ancient India','2006-02-15 05:03:42'),
+	 (1,2,'ANYTHING SAVANNAH: A Epic Story of a Pastry Chef And a Woman who must Chase a Feminist in An Abandoned Fun House','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'APACHE DIVINE: A Awe-Inspiring Reflection of a Pastry Chef And a Teacher who must Overcome a Sumo Wrestler in A U-Boat','2006-02-15 05:03:42'),
+	 (1,2,'APOCALYPSE FLAMINGOS: A Astounding Story of a Dog And a Squirrel who must Defeat a Woman in An Abandoned Amusement Park','2006-02-15 05:03:42'),
+	 (1,2,'APOLLO TEEN: A Action-Packed Reflection of a Crocodile And a Explorer who must Find a Sumo Wrestler in An Abandoned Mine Shaft','2006-02-15 05:03:42'),
+	 (1,2,'ARABIA DOGMA: A Touching Epistle of a Madman And a Mad Cow who must Defeat a Student in Nigeria','2006-02-15 05:03:42'),
+	 (1,2,'ARACHNOPHOBIA ROLLERCOASTER: A Action-Packed Reflection of a Pastry Chef And a Composer who must Discover a Mad Scientist in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'ARGONAUTS TOWN: A Emotional Epistle of a Forensic Psychologist And a Butler who must Challenge a Waitress in An Abandoned Mine Shaft','2006-02-15 05:03:42'),
+	 (1,2,'ARIZONA BANG: A Brilliant Panorama of a Mad Scientist And a Mad Cow who must Meet a Pioneer in A Monastery','2006-02-15 05:03:42'),
+	 (1,2,'ARK RIDGEMONT: A Beautiful Yarn of a Pioneer And a Monkey who must Pursue a Explorer in The Sahara Desert','2006-02-15 05:03:42'),
+	 (1,2,'ARMAGEDDON LOST: A Fast-Paced Tale of a Boat And a Teacher who must Succumb a Composer in An Abandoned Mine Shaft','2006-02-15 05:03:42'),
+	 (1,2,'ARMY FLINTSTONES: A Boring Saga of a Database Administrator And a Womanizer who must Battle a Waitress in Nigeria','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'ARSENIC INDEPENDENCE: A Fanciful Documentary of a Mad Cow And a Womanizer who must Find a Dentist in Berlin','2006-02-15 05:03:42'),
+	 (1,2,'ARTIST COLDBLOODED: A Stunning Reflection of a Robot And a Moose who must Challenge a Woman in California','2006-02-15 05:03:42'),
+	 (1,2,'ATLANTIS CAUSE: A Thrilling Yarn of a Feminist And a Hunter who must Fight a Technical Writer in A Shark Tank','2006-02-15 05:03:42'),
+	 (1,2,'ATTACKS HATE: A Fast-Paced Panorama of a Technical Writer And a Mad Scientist who must Find a Feminist in An Abandoned Mine Shaft','2006-02-15 05:03:42'),
+	 (1,2,'ATTRACTION NEWTON: A Astounding Panorama of a Composer And a Frisbee who must Reach a Husband in Ancient Japan','2006-02-15 05:03:42'),
+	 (1,2,'AUTUMN CROW: A Beautiful Tale of a Dentist And a Mad Cow who must Battle a Moose in The Sahara Desert','2006-02-15 05:03:42'),
+	 (1,2,'BABY HALL: A Boring Character Study of a A Shark And a Girl who must Outrace a Feminist in An Abandoned Mine Shaft','2006-02-15 05:03:42'),
+	 (1,2,'BACKLASH UNDEFEATED: A Stunning Character Study of a Mad Scientist And a Mad Cow who must Kill a Car in A Monastery','2006-02-15 05:03:42'),
+	 (1,2,'BADMAN DAWN: A Emotional Panorama of a Pioneer And a Composer who must Escape a Mad Scientist in A Jet Boat','2006-02-15 05:03:42'),
+	 (1,2,'BAKED CLEOPATRA: A Stunning Drama of a Forensic Psychologist And a Husband who must Overcome a Waitress in A Monastery','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'BALLOON HOMEWARD: A Insightful Panorama of a Forensic Psychologist And a Mad Cow who must Build a Mad Scientist in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'BALLROOM MOCKINGBIRD: A Thrilling Documentary of a Composer And a Monkey who must Find a Feminist in California','2006-02-15 05:03:42'),
+	 (1,2,'BANG KWAI: A Epic Drama of a Madman And a Cat who must Face a A Shark in An Abandoned Amusement Park','2006-02-15 05:03:42'),
+	 (1,2,'BANGER PINOCCHIO: A Awe-Inspiring Drama of a Car And a Pastry Chef who must Chase a Crocodile in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'BARBARELLA STREETCAR: A Awe-Inspiring Story of a Feminist And a Cat who must Conquer a Dog in A Monastery','2006-02-15 05:03:42'),
+	 (1,2,'BAREFOOT MANCHURIAN: A Intrepid Story of a Cat And a Student who must Vanquish a Girl in An Abandoned Amusement Park','2006-02-15 05:03:42'),
+	 (1,2,'BASIC EASY: A Stunning Epistle of a Man And a Husband who must Reach a Mad Scientist in A Jet Boat','2006-02-15 05:03:42'),
+	 (1,2,'BEACH HEARTBREAKERS: A Fateful Display of a Womanizer And a Mad Scientist who must Outgun a A Shark in Soviet Georgia','2006-02-15 05:03:42'),
+	 (1,2,'BEAR GRACELAND: A Astounding Saga of a Dog And a Boy who must Kill a Teacher in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'BEAST HUNCHBACK: A Awe-Inspiring Epistle of a Student And a Squirrel who must Defeat a Boy in Ancient China','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'BEAUTY GREASE: A Fast-Paced Display of a Composer And a Moose who must Sink a Robot in An Abandoned Mine Shaft','2006-02-15 05:03:42'),
+	 (1,2,'BED HIGHBALL: A Astounding Panorama of a Lumberjack And a Dog who must Redeem a Woman in An Abandoned Fun House','2006-02-15 05:03:42'),
+	 (1,2,'BEDAZZLED MARRIED: A Astounding Character Study of a Madman And a Robot who must Meet a Mad Scientist in An Abandoned Fun House','2006-02-15 05:03:42'),
+	 (1,2,'BEETHOVEN EXORCIST: A Epic Display of a Pioneer And a Student who must Challenge a Butler in The Gulf of Mexico','2006-02-15 05:03:42'),
+	 (1,2,'BEHAVIOR RUNAWAY: A Unbelieveable Drama of a Student And a Husband who must Outrace a Sumo Wrestler in Berlin','2006-02-15 05:03:42'),
+	 (1,2,'BENEATH RUSH: A Astounding Panorama of a Man And a Monkey who must Discover a Man in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'BERETS AGENT: A Taut Saga of a Crocodile And a Boy who must Overcome a Technical Writer in Ancient China','2006-02-15 05:03:42'),
+	 (1,2,'BETRAYED REAR: A Emotional Character Study of a Boat And a Pioneer who must Find a Explorer in A Shark Tank','2006-02-15 05:03:42'),
+	 (1,2,'BEVERLY OUTLAW: A Fanciful Documentary of a Womanizer And a Boat who must Defeat a Madman in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'BIKINI BORROWERS: A Astounding Drama of a Astronaut And a Cat who must Discover a Woman in The First Manned Space Station','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'BILKO ANONYMOUS: A Emotional Reflection of a Teacher And a Man who must Meet a Cat in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'BILL OTHERS: A Stunning Saga of a Mad Scientist And a Forensic Psychologist who must Challenge a Squirrel in A MySQL Convention','2006-02-15 05:03:42'),
+	 (1,2,'BINGO TALENTED: A Touching Tale of a Girl And a Crocodile who must Discover a Waitress in Nigeria','2006-02-15 05:03:42'),
+	 (1,2,'BIRCH ANTITRUST: A Fanciful Panorama of a Husband And a Pioneer who must Outgun a Dog in A Baloon','2006-02-15 05:03:42'),
+	 (1,2,'BIRD INDEPENDENCE: A Thrilling Documentary of a Car And a Student who must Sink a Hunter in The Canadian Rockies','2006-02-15 05:03:42'),
+	 (1,2,'BIRDCAGE CASPER: A Fast-Paced Saga of a Frisbee And a Astronaut who must Overcome a Feminist in Ancient India','2006-02-15 05:03:42'),
+	 (1,2,'BIRDS PERDITION: A Boring Story of a Womanizer And a Pioneer who must Face a Dog in California','2006-02-15 05:03:42'),
+	 (1,2,'BLACKOUT PRIVATE: A Intrepid Yarn of a Pastry Chef And a Mad Scientist who must Challenge a Secret Agent in Ancient Japan','2006-02-15 05:03:42'),
+	 (1,2,'BLADE POLISH: A Thoughtful Character Study of a Frisbee And a Pastry Chef who must Fight a Dentist in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'BLANKET BEVERLY: A Emotional Documentary of a Student And a Girl who must Build a Boat in Nigeria','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'BLINDNESS GUN: A Touching Drama of a Robot And a Dentist who must Meet a Hunter in A Jet Boat','2006-02-15 05:03:42'),
+	 (1,2,'BLOOD ARGONAUTS: A Boring Drama of a Explorer And a Man who must Kill a Lumberjack in A Manhattan Penthouse','2006-02-15 05:03:42'),
+	 (1,2,'BLUES INSTINCT: A Insightful Documentary of a Boat And a Composer who must Meet a Forensic Psychologist in An Abandoned Fun House','2006-02-15 05:03:42'),
+	 (1,2,'BOILED DARES: A Awe-Inspiring Story of a Waitress And a Dog who must Discover a Dentist in Ancient Japan','2006-02-15 05:03:42'),
+	 (1,2,'BONNIE HOLOCAUST: A Fast-Paced Story of a Crocodile And a Robot who must Find a Moose in Ancient Japan','2006-02-15 05:03:42'),
+	 (1,2,'BOOGIE AMELIE: A Lacklusture Character Study of a Husband And a Sumo Wrestler who must Succumb a Technical Writer in The Gulf of Mexico','2006-02-15 05:03:42'),
+	 (1,2,'BOONDOCK BALLROOM: A Fateful Panorama of a Crocodile And a Boy who must Defeat a Monkey in The Gulf of Mexico','2006-02-15 05:03:42'),
+	 (1,2,'BORN SPINAL: A Touching Epistle of a Frisbee And a Husband who must Pursue a Student in Nigeria','2006-02-15 05:03:42'),
+	 (1,2,'BORROWERS BEDAZZLED: A Brilliant Epistle of a Teacher And a Sumo Wrestler who must Defeat a Man in An Abandoned Fun House','2006-02-15 05:03:42'),
+	 (1,2,'BOULEVARD MOB: A Fateful Epistle of a Moose And a Monkey who must Confront a Lumberjack in Ancient China','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'BOUND CHEAPER: A Thrilling Panorama of a Database Administrator And a Astronaut who must Challenge a Lumberjack in A Baloon','2006-02-15 05:03:42'),
+	 (1,2,'BOWFINGER GABLES: A Fast-Paced Yarn of a Waitress And a Composer who must Outgun a Dentist in California','2006-02-15 05:03:42'),
+	 (1,2,'BRANNIGAN SUNRISE: A Amazing Epistle of a Moose And a Crocodile who must Outrace a Dog in Berlin','2006-02-15 05:03:42'),
+	 (1,2,'BRAVEHEART HUMAN: A Insightful Story of a Dog And a Pastry Chef who must Battle a Girl in Berlin','2006-02-15 05:03:42'),
+	 (1,2,'BREAKFAST GOLDFINGER: A Beautiful Reflection of a Student And a Student who must Fight a Moose in Berlin','2006-02-15 05:03:42'),
+	 (1,2,'BREAKING HOME: A Beautiful Display of a Secret Agent And a Monkey who must Battle a Sumo Wrestler in An Abandoned Mine Shaft','2006-02-15 05:03:42'),
+	 (1,2,'BRIDE INTRIGUE: A Epic Tale of a Robot And a Monkey who must Vanquish a Man in New Orleans','2006-02-15 05:03:42'),
+	 (1,2,'BRIGHT ENCOUNTERS: A Fateful Yarn of a Lumberjack And a Feminist who must Conquer a Student in A Jet Boat','2006-02-15 05:03:42'),
+	 (1,2,'BRINGING HYSTERICAL: A Fateful Saga of a A Shark And a Technical Writer who must Find a Woman in A Jet Boat','2006-02-15 05:03:42'),
+	 (1,2,'BROOKLYN DESERT: A Beautiful Drama of a Dentist And a Composer who must Battle a Sumo Wrestler in The First Manned Space Station','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'BROTHERHOOD BLANKET: A Fateful Character Study of a Butler And a Technical Writer who must Sink a Astronaut in Ancient Japan','2006-02-15 05:03:42'),
+	 (1,2,'BUBBLE GROSSE: A Awe-Inspiring Panorama of a Crocodile And a Moose who must Confront a Girl in A Baloon','2006-02-15 05:03:42'),
+	 (1,2,'BUCKET BROTHERHOOD: A Amazing Display of a Girl And a Womanizer who must Succumb a Lumberjack in A Baloon Factory','2006-02-15 05:03:42'),
+	 (1,2,'BUGSY SONG: A Awe-Inspiring Character Study of a Secret Agent And a Boat who must Find a Squirrel in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'BULL SHAWSHANK: A Fanciful Drama of a Moose And a Squirrel who must Conquer a Pioneer in The Canadian Rockies','2006-02-15 05:03:42'),
+	 (1,2,'BULWORTH COMMANDMENTS: A Amazing Display of a Mad Cow And a Pioneer who must Redeem a Sumo Wrestler in The Outback','2006-02-15 05:03:42'),
+	 (1,2,'BUNCH MINDS: A Emotional Story of a Feminist And a Feminist who must Escape a Pastry Chef in A MySQL Convention','2006-02-15 05:03:42'),
+	 (1,2,'BUTCH PANTHER: A Lacklusture Yarn of a Feminist And a Database Administrator who must Face a Hunter in New Orleans','2006-02-15 05:03:42'),
+	 (1,2,'BUTTERFLY CHOCOLAT: A Fateful Story of a Girl And a Composer who must Conquer a Husband in A Shark Tank','2006-02-15 05:03:42'),
+	 (1,2,'CABIN FLASH: A Stunning Epistle of a Boat And a Man who must Challenge a A Shark in A Baloon Factory','2006-02-15 05:03:42');
+INSERT INTO vk.messages (from_user_id,to_user_id,body,created_at) VALUES
+	 (1,2,'CADDYSHACK JEDI: A Awe-Inspiring Epistle of a Woman And a Madman who must Fight a Robot in Soviet Georgia','2006-02-15 05:03:42'),
+	 (1,2,'CALENDAR GUNFIGHT: A Thrilling Drama of a Frisbee And a Lumberjack who must Sink a Man in Nigeria','2006-02-15 05:03:42'),
+	 (1,2,'CALIFORNIA BIRDS: A Thrilling Yarn of a Database Administrator And a Robot who must Battle a Database Administrator in Ancient India','2006-02-15 05:03:42'),
+	 (1,2,'CAMELOT VACATION: A Touching Character Study of a Woman And a Waitress who must Battle a Pastry Chef in A MySQL Convention','2006-02-15 05:03:42'),
+	 (1,2,'CAMPUS REMEMBER: A Astounding Drama of a Crocodile And a Mad Cow who must Build a Robot in A Jet Boat','2006-02-15 05:03:42'),
+	 (1,2,'CANDIDATE PERDITION: A Brilliant Epistle of a Composer And a Database Administrator who must Vanquish a Mad Scientist in The First Manned Space Station','2006-02-15 05:03:42'),
+	 (1,2,'CANDLES GRAPES: A Fanciful Character Study of a Monkey And a Explorer who must Build a Astronaut in An Abandoned Fun House','2006-02-15 05:03:42'),
+	 (1,2,'CANYON STOCK: A Thoughtful Reflection of a Waitress And a Feminist who must Escape a Squirrel in A Manhattan Penthouse','2006-02-15 05:03:42'),
+	 (1,2,'CAPER MOTIONS: A Fateful Saga of a Moose And a Car who must Pursue a Woman in A MySQL Convention','2006-02-15 05:03:42'),
+	 (1,2,'CARIBBEAN LIBERTY: A Fanciful Tale of a Pioneer And a Technical Writer who must Outgun a Pioneer in A Shark Tank','2006-02-15 05:03:42');
+	
+	
